3e300fdca7093d7559d8b63dd84d16756fcdd83c
AndesAndedia <100776746+AndesAndedia@users.noreply.github.com>
2023-06-10 16:05:43 +0700

    cleaning the code

--- coba.php
+++ coba.php
@@ -65,7 +65,7 @@ require 'head.php'; ?>
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Logo -->
             <a href="#" class="brand-link">
-                <img src="assets/logo.jpeg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="assets/logo.jpeg" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MoovOn</span>
             </a>
 
@@ -74,7 +74,7 @@ require 'head.php'; ?>
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="assets/user.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
--- formeditpelanggan.php
+++ formeditpelanggan.php
@@ -40,7 +40,7 @@ $data = mysqli_fetch_array($query);
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title>MoovOn | Edit Pelanggan</title>
 
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -95,7 +95,7 @@ $data = mysqli_fetch_array($query);
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Logo -->
       <a href="#" class="brand-link">
-        <img src="assets/logo.jpeg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="assets/logo.jpeg" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">MoovOn</span>
       </a>
 
@@ -104,7 +104,7 @@ $data = mysqli_fetch_array($query);
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="assets/user.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
